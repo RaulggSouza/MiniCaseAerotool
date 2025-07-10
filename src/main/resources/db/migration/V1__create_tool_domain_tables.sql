20bbcb08b3ca555e3768df834b9793132f4b3c1e
RaulggSouza <raulggsouza@hotmail.com>
2025-07-10 19:15:16 -0300

    refactor: Aplicando sugestões, sem update ainda

--- src/main/resources/db/migration/V1__create_tool_domain_tables.sql
+++ src/main/resources/db/migration/V1__create_tool_domain_tables.sql
@@ -2,34 +2,34 @@
 
 CREATE TABLE tool(
     id BIGSERIAL PRIMARY KEY,
-    integration_id VARCHAR(36) NOT NULL,
+    integration_id VARCHAR(36),
     description TEXT,
     category VARCHAR(100) NOT NULL,
-    created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
-    deleted_at TIMESTAMPTZ
+    created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    updated_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    deleted_at TIMESTAMPTZ DEFAULT NULL
 );
 
 CREATE TABLE tool_item_status(
     id BIGSERIAL PRIMARY KEY,
     name TEXT NOT NULL,
     description TEXT,
-    created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
-    deleted_at TIMESTAMPTZ
+    created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    updated_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    deleted_at TIMESTAMPTZ DEFAULT NULL
 );
 
 CREATE TABLE tool_item(
     id BIGSERIAL PRIMARY KEY,
-    integration_id VARCHAR(36) NOT NULL,
+    integration_id VARCHAR(36),
     color VARCHAR(50),
     tool_id BIGINT NOT NULL,
     status_id BIGINT NOT NULL,
-    CONSTRAINT fk_tool_item_to_tool FOREIGN KEY(tool_id) REFERENCES tool(id),
-    CONSTRAINT fk_tool_item_to_status FOREIGN KEY(status_id) REFERENCES tool_item_status(id),
-    created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
-    deleted_at TIMESTAMPTZ
+    CONSTRAINT tool_item_tool_id_tool_fkey FOREIGN KEY(tool_id) REFERENCES tool(id),
+    CONSTRAINT tool_item_status_id_tool_item_status_fkey FOREIGN KEY(status_id) REFERENCES tool_item_status(id),
+    created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    updated_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    deleted_at TIMESTAMPTZ DEFAULT NULL
 );
 
 CREATE OR REPLACE FUNCTION update_updated_at()
