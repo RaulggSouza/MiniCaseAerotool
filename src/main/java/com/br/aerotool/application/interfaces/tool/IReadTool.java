e14f176363f00f4d233e4eec24707f512b47b5c0
Fernando De Riggi <Fernando_De_Riggi@outlook.com>
2025-07-14 19:10:05 -0300

    feat(tool): implementação completa do ReadTool, com busca de filtros

--- src/main/java/com/br/aerotool/application/interfaces/tool/IReadTool.java
+++ src/main/java/com/br/aerotool/application/interfaces/tool/IReadTool.java
@@ -4,4 +4,5 @@ import com.br.aerotool.application.interfaces.IReadEntity;
 import com.br.aerotool.domain.entities.Tool;
 
 public interface IReadTool extends IReadEntity<Tool, Long> {
+
 }
---
+++ src/main/java/com/br/aerotool/incoming/rest/model/tool/request/ToolFilterRequest.java
@@ -0,0 +1,18 @@
+package com.br.aerotool.incoming.rest.model.tool.request;
+
+import com.br.aerotool.domain.entities.ToolCategory;
+
+import java.time.LocalDateTime;
+
+public record ToolFilterRequest(
+        Long id,
+        String integrationId,
+        String description,
+        ToolCategory category,
+        LocalDateTime startDate,
+        LocalDateTime endDate,
+        Boolean deleted,
+        Integer page,
+        Integer size
+)
+{}
