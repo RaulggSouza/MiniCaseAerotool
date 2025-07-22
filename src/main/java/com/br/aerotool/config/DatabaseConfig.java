e3a3c87f4d21580f982fc479af9ec6415de6dedd
Fernando De Riggi <Fernando_De_Riggi@outlook.com>
2025-07-22 01:06:58 -0300

    feat: criação do DataBaseConfig

--- src/main/java/com/br/aerotool/config/DatabaseConfig.java
+++ src/main/java/com/br/aerotool/config/DatabaseConfig.java
@@ -1,4 +1,49 @@
 package com.br.aerotool.config;
 
-public class DatabaseConfig {
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.Optional;
+
+public final class DatabaseConfig {
+
+    private static final HikariDataSource dataSource;
+
+    static {
+        try {
+            HikariConfig config = new HikariConfig();
+
+            String dbName = getEnvOrDefault("POSTGRES_DB", "aerotool_dev");
+            String dbUser = getEnvOrDefault("DATABASE_USERNAME", "dev_user");
+            String dbPassword = getEnvOrDefault("DATABASE_PASSWORD", "dev_pass");
+
+            String dbUrl = "jdbc:postgresql://localhost:5432/" + dbName;
+
+            config.setJdbcUrl(dbUrl);
+            config.setUsername(dbUser);
+            config.setPassword(dbPassword);
+
+            config.setMaximumPoolSize(10);
+            config.setMinimumIdle(5);
+            config.setConnectionTimeout(30000);
+
+            dataSource = new HikariDataSource(config);
+
+        } catch (Exception e) {
+            System.err.println("Falha fatal ao inicializar o pool de conexões: " + e.getMessage());
+            throw new RuntimeException("Erro na configuração do DataSource", e);
+        }
+    }
+
+    private static String getEnvOrDefault(String name, String defaultValue) {
+        return Optional.ofNullable(System.getenv(name)).orElse(defaultValue);
+    }
+
+    public static Connection getConnection() throws SQLException {
+        return dataSource.getConnection();
+    }
+
+    private DatabaseConfig() {}
 }
--- src/main/java/com/br/aerotool/outgoing/persistence/repositories/ToolRepository.java
+++ src/main/java/com/br/aerotool/outgoing/persistence/repositories/ToolRepository.java
@@ -1,4 +1,36 @@
 package com.br.aerotool.outgoing.persistence.repositories;
 
-public class ToolRepository {
+import com.br.aerotool.domain.entities.tool.Tool;
+import com.br.aerotool.domain.repositories.IToolRepository;
+import com.br.aerotool.incoming.rest.model.tool.request.ToolRequest;
+import com.br.aerotool.incoming.rest.model.tool.response.ToolResponse;
+
+import java.util.List;
+import java.util.Optional;
+
+public class ToolRepository implements IToolRepository {
+    @Override
+    public void create(ToolRequest request) {
+
+    }
+
+    @Override
+    public void delete(Long id) {
+
+    }
+
+    @Override
+    public Optional<Tool> findById(Long id) {
+        return Optional.empty();
+    }
+
+    @Override
+    public List<Tool> findAll() {
+        return List.of();
+    }
+
+    @Override
+    public void update(ToolResponse toolResponse) {
+
+    }
 }
